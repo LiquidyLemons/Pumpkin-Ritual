13aac4bf0e2d581b861b5748b55577e623538308
Madeline <wonderbrayden@gmail.com>
2024-10-09 13:00:42 -0400

    did thnigs

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,10 @@
-extends CharacterBody2D
+extends CharacterBody2D 
 
 @export var speedX = 300
 @export var gravity = 30
 @export var jump_force = 600
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if (!is_on_floor() && velocity.y < 500):
 		velocity.y += gravity
 	
@@ -14,3 +14,10 @@ func _physics_process(delta):
 	var dirX = Input.get_axis("move_left", "move_right")
 	velocity.x = speedX * dirX
 	move_and_slide()
+	
+	skew = 0.2 * -dirX # silly (delete)
+
+	# send pumpky back (TEMP)
+	if (position.y > 800):
+		position.y = 100
+		position.x = 300
